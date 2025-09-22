edfb10ede33331eb7519df2a3510743982e9d2b1
Sri200279 <164613160+Sri200279@users.noreply.github.com>
2025-09-22 17:01:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ const questionsbysubject = {
    java: [
     {
       id: 1,
-      Question: "In BorderLayout,how many region can we add components into?",
+      Question: "1. In BorderLayout,how many region can we add components into?",
       Options: ["3", "4", "5", "Unlimited"],
     },
     {
       id: 2,
-      Question: "Which SQL clause is used to filter records?",
+      Question: "2. Which SQL clause is used to filter records?",
       Options: ["WHERE", "ORDERBY", "GROUPBY", "HAVING"],
     },
     {
       id: 3,
-      Question: "Loop output?" +
+      Question: "3. Loop output?" +
       "for(int i=1;i<=5;i++)"+
       "{ if(i==3) break;"+
       "System.out.print(i); }",
@@ -28,7 +28,7 @@ const questionsbysubject = {
     },
     {
       id: 4,
-      Question: "Inheritance field output?"+
+      Question: "4. Inheritance field output?"+
       "class A{int x=10;}"+"class B extends A{int x=20;"+
       "void print(){System.out.println(super.x+' '+x);}"+
       "public static void main(String[] a){ new B().print(); }}",
@@ -36,61 +36,61 @@ const questionsbysubject = {
     },
     {
       id: 5,
-      Question: "String pool check? "+`String s1="hello", s2="hello", s3=new String("hello");`+
+      Question: "5. String pool check? "+`String s1="hello", s2="hello", s3=new String("hello");`+
         `System.out.println((s1==s2)+" "+(s1==s3));`,
       Options: ["true false", "false true", "false false", "true true"],
     },
     {
   id: 6,
-  Question: "What will be the output?\nint x = 10;\nint y = 20;\nSystem.out.println(x++ + ++y);",
+  Question: "6. What will be the output?\nint x = 10;\nint y = 20;\nSystem.out.println(x++ + ++y);",
   Options: ["30", "31", "32", "33"],
 },
 
 {
   id: 7,
-  Question: "String pool check?\nString s1 = \"hello\", s2 = \"hello\", s3 = new String(\"hello\");\nSystem.out.println((s1==s2)+\" \"+(s1==s3));",
+  Question: "7. String pool check?\nString s1 = \"hello\", s2 = \"hello\", s3 = new String(\"hello\");\nSystem.out.println((s1==s2)+\" \"+(s1==s3));",
   Options: ["true false", "false true", "false false", "true true"],
 },
 
 {
   id: 8,
-  Question: "What is the default value of a boolean variable in Java?",
+  Question: "8. What is the default value of a boolean variable in Java?",
   Options: ["true", "false", "null", "0"],
 },
 
 {
   id: 9,
-  Question: "Which keyword is used to inherit a class in Java?",
+  Question: "9. Which keyword is used to inherit a class in Java?",
   Options: ["this", "super", "extends", "implements"],
 },
 
 {
   id: 10,
-  Question: "Which of these is not a Java feature?",
+  Question: "10. Which of these is not a Java feature?",
   Options: ["Object-Oriented", "Use of Pointers", "Portable", "Robust"],
 },
 
 {
   id: 11,
-  Question: "What will be the output?\nSystem.out.println(10 + 20 + \"Java\");",
+  Question: "11. What will be the output?\nSystem.out.println(10 + 20 + \"Java\");",
   Options: ["30Java", "Java30", "1020Java", "Java1020"],
 },
 
 {
   id: 12,
-  Question: "Which of the following is not a valid access modifier in Java?",
+  Question: "12. Which of the following is not a valid access modifier in Java?",
   Options: ["public", "private", "protected", "friendly"],
 },
 
 {
   id: 13,
-  Question: "What will be the output?\nSystem.out.println(\"A\" + \"B\" + 'A' + 'B');",
+  Question: "13. What will be the output?\nSystem.out.println(\"A\" + \"B\" + 'A' + 'B');",
   Options: ["ABAB", "ABA66", "AB131", "ABAB131"],
 },
 
 {
   id: 14,
-  Question: "Which of the following is true about constructors in Java?",
+  Question: "14. Which of the following is true about constructors in Java?",
   Options: [
     "Constructor must have return type",
     "Constructor name must be same as class name",
@@ -101,7 +101,7 @@ const questionsbysubject = {
 
 {
   id: 15,
-  Question: "Which of these is the correct way to declare a main method in Java?",
+  Question: "15. Which of these is the correct way to declare a main method in Java?",
   Options: [
     "public static void main(String args[])",
     "public void main(String args[])",
@@ -112,17 +112,17 @@ const questionsbysubject = {
 
     {
       id: 16,
-      Question: "Read file line by line?",
+      Question: "16. Read file line by line?",
       Options: ["FileReader", "BufferedReader", "Scanner", "ObjectInputStream"],
     },
 {
   id: 17,
-  Question: "Which of the following are primitive types?",
+  Question: "17. Which of the following are primitive types?",
   Options: ["byte", "String", "integer", "Float"],
 },
 {
   id: 18,
-  Question: "What are primitive data types in Java?",
+  Question: "18. What are primitive data types in Java?",
   Options: [
     "int, float, String, double",
     "byte, short, int, long, float, double, boolean, char",
@@ -133,13 +133,13 @@ const questionsbysubject = {
 
 {
   id: 19,
-  Question: "Which method is used to execute a SQL query that returns a ResultSet in JDBC?",
+  Question: "19. Which method is used to execute a SQL query that returns a ResultSet in JDBC?",
   Options: ["executeUpdate()", "executeQuery()", "execute()", "executeResult()"],
 },
 
 {
   id: 20,
-  Question: "Which of these is NOT a valid type of JDBC driver?",
+  Question: "20. Which of these is NOT a valid type of JDBC driver?",
   Options: [
     "JDBC-ODBC Bridge driver",
     "Native-API driver",
@@ -150,19 +150,19 @@ const questionsbysubject = {
 
 {
   id: 21,
-  Question: "Which JDBC interface is used for precompiled SQL statements?",
+  Question: "21. Which JDBC interface is used for precompiled SQL statements?",
   Options: ["Statement", "PreparedStatement", "CallableStatement", "ResultSet"],
 },
 
 {
   id: 22,
-  Question: "What will be the output?\nArrayList<String> list = new ArrayList<>();\nlist.add(\"A\"); list.add(\"B\"); list.add(1, \"C\");\nSystem.out.println(list);",
+  Question: "22. What will be the output?\nArrayList<String> list = new ArrayList<>();\nlist.add(\"A\"); list.add(\"B\"); list.add(1, \"C\");\nSystem.out.println(list);",
   Options: ["[A, B, C]", "[A, C, B]", "[C, A, B]", "[B, A, C]"],
 },
 
 {
   id: 23,
-  Question: "Which of these is correct about ArrayList in Java?",
+  Question: "23. Which of these is correct about ArrayList in Java?",
   Options: [
     "It is synchronized",
     "It allows duplicate elements",
@@ -172,42 +172,42 @@ const questionsbysubject = {
 },
 {
   id: 24,
-  Question: "What will be the output?\nArrayList<Integer> list = new ArrayList<>();\nlist.add(1);\nlist.add(2);\nlist.remove(1);\nSystem.out.println(list);",
+  Question: "24. What will be the output?\nArrayList<Integer> list = new ArrayList<>();\nlist.add(1);\nlist.add(2);\nlist.remove(1);\nSystem.out.println(list);",
   Options: ["[2]", "[1]", "[ ]", "[1, 2]"],
 },
 
 {
   id: 25,
-  Question: "Which class is used to create a client socket connection in java.net?",
+  Question: "25. Which class is used to create a client socket connection in java.net?",
   Options: ["ServerSocket", "Socket", "DatagramSocket", "InetAddress"],
 },
 
 {
   id: 26,
-  Question: "Which java.net class is used to represent an IP address?",
+  Question: "26. Which java.net class is used to represent an IP address?",
   Options: ["InetAddress", "URL", "SocketAddress", "URI"],
 },
 
 {
   id: 27,
-  Question: "Which protocol is supported by the URL class in java.net by default?",
+  Question: "27. Which protocol is supported by the URL class in java.net by default?",
   Options: ["ftp", "http", "https", "smtp"],
 },
 
 {
   id: 28,
-  Question: "Which method of URLConnection is used to open a connection for reading?",
+  Question: "28. Which method of URLConnection is used to open a connection for reading?",
   Options: ["getStream()", "openStream()", "getInputStream()", "read()"],
 },
 
 {
   id: 29,
-  Question: "Which method of Socket class returns the OutputStream to send data?",
+  Question: "29. Which method of Socket class returns the OutputStream to send data?",
   Options: ["getOutputStream()", "openOutput()", "sendStream()", "write()"],
 },
 {
   id: 30,
-  Question: "Which class is used to bind a ServerSocket to a specific port?",
+  Question: "30. Which class is used to bind a ServerSocket to a specific port?",
   Options: ["Socket", "DatagramSocket", "ServerSocket", "InetAddress"],
 },
   ],
@@ -1045,4 +1045,5 @@ export default function App() {
        
 
 }
+
 
